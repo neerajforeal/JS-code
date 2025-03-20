25a28573b776f6494e927abfe59d5cab2f6d1f67
neeraj <neerajpallikonda34@gmail.com>
2025-03-20 11:53:29 +0530

    data types

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+let name = "Yash";
+let age = 78;
+let isLoggedIn = false;
+let state; // undefined value
+let city = null; // null value
+
+// number => 2 to pwr 53 (range)
+// bigint
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique value
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof null); // => object
+console.log(typeof state); // => undefined
