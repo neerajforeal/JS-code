92b8940228be163bec12d8bf1ad92e31c073009f
neeraj <neerajpallikonda34@gmail.com>
2025-03-20 15:06:35 +0530

    key operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -33,3 +33,32 @@ let someNumber = 32;
 let stringNumber = String(someNumber);
 console.log(stringNumber); // => "32"
 console.log(typeof stringNumber); // => string
+
+// OPERATIONS
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue); // => -3
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+console.log(str3); // => "hello hitesh"
+
+console.log("1" + 2); // => "12"
+console.log(1 + "2"); // => "12"
+console.log("1" + 2 + 2); // => "122"
+console.log(1 + 2 + "2"); // => "32"
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); // => 101
+
+// postfix increment
+let x = 3;
+const y = x++; // => x = 4, y = 3
+
+// prefix increment
+let x2 = 3;
+const y2 = ++x2; // => x2 = 4, y2 = 4
