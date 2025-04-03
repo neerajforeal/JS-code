eebe3524d85ee37687336bfab48e46cbcfbcc455
neeraj <neerajpallikonda34@gmail.com>
2025-04-03 15:38:33 +0530

    this keyword and constructor function

---
+++ OOP/script.js
@@ -0,0 +1,34 @@
+const user = {
+    username: 'henry',
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database")
+        //console.log(`Username: ${this.username}`)
+        console.log(this)
+    }
+}
+
+console.log(user.username)
+// console.log(user.getUserDetails())
+console.log(this)
+
+
+// constructor function
+function myuser(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function() {
+        console.log(`Welcome ${this.username}`)
+    }
+
+    return this
+}
+const userOne = new myuser('henry', 12, true)
+const userTwo = new myuser('Jack', 11, false)
+console.log(userOne)
+console.log(userTwo)
+
